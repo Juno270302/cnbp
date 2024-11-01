51781577868664417cd8822f33b80167a76c2e71
Juno270302 <hungnxb2703@gmail.com>
2024-11-01 11:31:38 +0700

    har

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./i18n.ts");
+
+const nextConfig = {};
+
+export default withNextIntl(nextConfig);
